d7bb9cb7831aee413d50874226a1b751a6446136
Jomiel <enriquez.jliquigan@gmail.com>
2023-02-17 23:59:01 +0800

    update blog

--- blog/ListOfBlog.js
+++ blog/ListOfBlog.js
@@ -7,10 +7,11 @@ const ListOfBlog = [
             <img src="./blog/images/GodMode[1].png" class="img-fluid"/>
             <br/>
             <br/>
-            <div>
+            <div style="font-size:20px;">
                 If you want to have access in every settings in windows as you can see in the image above, you should follow these steps.
+                <br/><br/>
+                1.  Create a new folder and set its name to <strong>"GodMode.{ED7BA470-8E54-465E-825C-99712043E01C}"</strong>
                 <br/>
-                <p> Create a new folder and set its name to <strong>GodMode.{ED7BA470-8E54-465E-825C-99712043E01C}</strong>.</p>
             </div>
             
             
